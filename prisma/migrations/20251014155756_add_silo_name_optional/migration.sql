ccf19c25376d78be3bcee534691be6789042432c
AlcilenySilva <capic.2023118tads0045@aluno.ifpi.edu.br>
2025-10-14 14:33:34 -0300

    Erro BD

--- prisma/migrations/20251014155756_add_silo_name_optional/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."silo_data" (
-    "id" SERIAL NOT NULL,
-    "silo_name" TEXT,
-    "distancia_cm" DOUBLE PRECISION NOT NULL,
-    "dataHora" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "silo_data_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20251014160239_nome_obrigatorio/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `silo_name` on table `silo_data` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "public"."silo_data" ALTER COLUMN "silo_name" SET NOT NULL,
-ALTER COLUMN "silo_name" SET DEFAULT 'Desconhecido';
